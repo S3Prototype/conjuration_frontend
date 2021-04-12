b0a1271772dc361747e84736784a2bf9f77fb549
S3Prototype <liuqahs15@gmail.com>
2021-04-12 12:43:06 -0400

    Added avner and goldsworthy examples

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
           description={`You want a professional, high-end website, because you want your customers to respect your brand. We provide custom-made, professional Shopify storefronts, Wordpress sites and lead-generating Landing pages that will make your small business look like a Fortune 500 Company.`}
           portfolioType={"Website"}
           phoneNumber={"973-914-7775"}
-          image={"https://res.cloudinary.com/diujqlncs/image/upload/v1616195820/conjuration_frontent/puppiclown_c0yemt.png"}
+          image={"https://res.cloudinary.com/diujqlncs/image/upload/v1618245510/conjuration_frontent/avner_khqgz2.png"}
       />
 
       <PortfolioSection 
